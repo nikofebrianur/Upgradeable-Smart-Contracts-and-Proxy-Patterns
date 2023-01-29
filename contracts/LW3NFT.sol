4ff262eda2abf5f8c5faad8d73b4bb8ffd297dd7
Niko Febrianur <nikofebrianur@gmail.com>
2023-01-29 19:52:00 +0800

    contract 1

---
+++ contracts/LW3NFT.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.17;
+
+import "@openzeppelin/contracts-upgradeable/token/ERC721/ERC721Upgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+
+contract LW3NFT is
+    Initializable,
+    ERC721Upgradeable,
+    UUPSUpgradeable,
+    OwnableUpgradeable
+{
+    function initialize() public initializer {
+        __ERC721_init("LW3NFT", "LW3NFT");
+        __Ownable_init();
+        _mint(msg.sender, 1);
+    }
+
+    function _authorizeUpgrade(address newImplementation)
+        internal
+        override
+        onlyOwner
+    {}
+}
